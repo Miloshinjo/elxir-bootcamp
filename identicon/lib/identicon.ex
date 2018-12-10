4cc263ee95df1de0393819797fe9cb45abd4230b
milos.dzeletovic <milos.dzeletovic@consulteer.com>
2018-12-10 09:42:08 +0100

    Introduce alias syntax

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,26 +1,28 @@
 defmodule Identicon do
+
+  alias Identicon.Image
+
   def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
     |> filter_odd_squares
-    |> build_pixel_map
+  #   |> build_pixel_map
   end
 
-  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    
-  end
+  # def build_pixel_map(%Image{grid: grid} = image) do
+  # end
 
-  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+  def filter_odd_squares(%Image{grid: grid} = image) do
     grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0 # rem - remainder, works like modulo in js.
     end
 
-    %Identicon.Image{image | grid: grid}
+    %Image{image | grid: grid}
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = image) do
+  def build_grid(%Image{hex: hex} = image) do
     grid =
       hex
       |> Enum.chunk(3) # check the hex into lists of 3
@@ -28,7 +30,7 @@ defmodule Identicon do
       |> List.flatten # take a nested list and flatten all the values in it. (avoid lists within lists)
       |> Enum.with_index # give us the idea of an list index in our iterations. It will turn all elements into 2 element tuples, with second value being the index.
 
-    %Identicon.Image{image | grid: grid} # return a struct with all image props plus grid.
+    %Image{image | grid: grid} # return a struct with all image props plus grid.
   end
 
   def mirror_row(row) do
@@ -37,14 +39,14 @@ defmodule Identicon do
     row ++ [second, first]
   end
 
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
+  def pick_color(%Image{hex: [r, g, b | _tail]} = image) do
+    %Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
-    %Identicon.Image{hex: hex}
+    %Image{hex: hex}
   end
 end
